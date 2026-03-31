df5bf538de56e46fd56b47bc73fb7fcb7a1f38f3
Jess Sullivan <jess@sulliwood.org>
2026-03-31 01:59:54 -0400

    fix(nix): add pandoc, set NIX_CFLAGS for glibc headers in Zig sandbox

--- nix/libghostty.nix
+++ nix/libghostty.nix
@@ -32,7 +32,7 @@ in
     src = ghosttySrc;
 
     nativeBuildInputs = [
-      git ncurses pkg-config zig_0_15
+      git ncurses pkg-config zig_0_15 pkgs.pandoc
       gobject-introspection wayland-scanner wayland-protocols
     ];
     inherit buildInputs;
@@ -48,6 +48,10 @@ in
       export ZIG_LOCAL_CACHE_DIR="$TMPDIR/zig-cache"
       export ZIG_GLOBAL_CACHE_DIR="$TMPDIR/zig-global"
       export HOME="$TMPDIR"
+
+      # Point Zig's C compiler at the Nix-provided glibc headers
+      export NIX_CFLAGS_COMPILE="''${NIX_CFLAGS_COMPILE:-} -isystem ${pkgs.glibc.dev}/include"
+      export NIX_LDFLAGS="''${NIX_LDFLAGS:-} -L${pkgs.glibc}/lib"
 
       zig build \
         --system ${deps} \
